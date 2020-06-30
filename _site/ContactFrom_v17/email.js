c3e629dfd877501580c66662fbf31ece52e50d69
Amritpal Singh <matharooamrit098@gmail.com>
2020-07-01 00:11:23 +0530

    Sending Email to Client

--- _site/ContactFrom_v17/email.js
+++ _site/ContactFrom_v17/email.js
@@ -1,39 +1,44 @@
 var final=""
+var clientEmail="matharooamrit0098@gmail.com"
+var clientFinal=""
+var orderPlaced="Order Placed"
+var newOrder="New Order"
 function myFunction() {
-  var first_name= document.getElementById("first-name").value;
-  var last_name= document.getElementById("last-name").value;
-  var email= document.getElementById("email").value;
-  var phone= document.getElementById("phone").value;
-  var address= document.getElementById("address").value;
-  var state = document.getElementById("state").value;
-  var city= document.getElementById("city").value;
-  var pincode= document.getElementById("pincode").value;
-
+  var first_name= String(document.getElementById("first-name").value);
+  var last_name= String(document.getElementById("last-name").value);
+  var email= String(document.getElementById("email").value);
+  var phone= String(document.getElementById("phone").value);
+  var address= String(document.getElementById("address").value);
+  var state = String(document.getElementById("state").value);
+  var city= String(document.getElementById("city").value);
+  var pincode= String(document.getElementById("pincode").value);
   var order= JSON.parse(localStorage.getItem("__cart"));
 	var product="";
-  var productIncart="\n\nCartItems\n";
+  var productIncart="";
   var total=0;
 	for( let i=0;i<order.length;i+=1){
     total=total+(order[i].price*order[i].quantity)
-		product= "Name : "+String(order[i].name)+" Quantity : "+String(order[i].quantity)+" Price : "+String(order[i].price);
-		productIncart=productIncart+product+"\n";
+		product= "<h2>Name : "+String(order[i].name)+"</h2><h2>Quantity : "+String(order[i].quantity)+"</h2><h2>Price : "+String(order[i].price)+"</h2>";
+		productIncart=productIncart+product+"<br></br>";
   }
-  productIncart=productIncart+"Total : "+String(total)+"\n"
-final="First Name : "+String(first_name)+"\nLast Name : "+String(last_name)+"\nEmail : "+String(email)+"\nPhone : "+String(phone)+"\nAddress : "+String(address)+"\nState : "+String(state)+"\nCity : "+String(city)+"\nPin Code : "+String(pincode)+productIncart
-console.log(final);
-  // sendEmail()
+  productIncart=productIncart+"<h2>Total : "+String(total)+"</h2>"
+final="<html><h2>Name : "+first_name+" "+last_name+"</h2><h2>Email : "+email+"</h2><h2>Phone : "+phone+"</h2><h2>Address : "+address+"</h2><h2>State : "+state+"</h2><h2>City : "+city+"</h2><h2>Pin Code : "+pincode+"</h2><br></br><h1>Order Details</h1>"+productIncart+"</html>"
+console.log(email);
+  clientFinal="<html><h1>Your Order has been placed</h1><h1>Order Details</h1>"+productIncart+"</html>"
+  sendEmail(clientEmail,final,newOrder)
+  sendEmail(email,final,orderPlaced)
   localStorage.clear()
   }
 
-  function sendEmail() {
+  function sendEmail(x,y,z) {
 	Email.send({
 	Host: "smtp.gmail.com",
 	Username : "matharooamrit0098@gmail.com",
 	Password : "grandmax#2846",
-	To : 'matharooamrit0098@gmail.com',
+	To : x,
 	From : "matharooamrit0098@gmail.com",
-	Subject : "Form Data",
-	Body : final,
+	Subject : z,
+	Body : y,
 	}).then(
 		message => alert("mail sent successfully")
 	);
